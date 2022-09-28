f94e02b97316b2a84bb9835d4e90460378be1102
xSharkhy <sharkhyacc@gmail.com>
2022-09-28 19:57:45 +0200

    landing!

--- DAW/DWES/Actividad__5/ordenaArrayIsmael.php
+++ DAW/DWES/Actividad__5/ordenaArrayIsmael.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ismael Morejón</title>
-</head>
-<body>
 <?php
-$intArray = [29, 64, 69, 89, 29, 70, 89, 48, 74, 55];
+$intArray = [29, 64, 69, 98, 32, 70, 89, 48, 74, 55];
 foreach ($intArray as $value) echo $value . " ";
 
 echo "<br>";
@@ -25,8 +16,4 @@ for ($i = 0; $i < count($intArray) - 1; $i++) {
     $intArray[$i] = $aux;
 }
 
-
 foreach ($intArray as $value) echo $value . " ";
-?>
-</body>
-</html>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="imatges/luffy.jpg" type="image/x-icon">
-    <title>Ismael Morejón</title>
+    <title>DAW 2022/23</title>
 </head>
 
 <body id="topPage">
@@ -15,26 +15,29 @@
     <main>
         <article>
             <header>
-                <h1>ISMAEL MOREJÓN</h1>
-                <p>2º de DAW</p>
+                <h1>2DAW 2022 / 23</h1>
             </header>
             <section class="content">
-                <div>
-                    <img src="imatges/git.svg" alt="Git" height=120>
-                    <img src="imatges/html5.svg" alt="HTML5" height=120>
-                    <img src="imatges/java.svg" alt="Java" height=120>
-                    <img src="imatges/js.svg" alt="Javascript" height=120>
-                    <img src="imatges/linux.svg" alt="Linux" height=120>
-                    <img src="imatges/mysql.svg" alt="MySQL" height=120>
-                    <img src="imatges/php.svg" alt="PHP" height=120>
-                    <img src="imatges/python.svg" alt="Python" height=120>
-                </div>
-                <div>
-                    <img align="center" height=142
-                        src="https://github-readme-stats.vercel.app/api/top-langs/?username=xSharkhy&theme=midnight-purple&layout=compact&bg_color=0D1117&hide_border=true" />
-                    <img align="center" height=140
-                        src="https://github-readme-stats.vercel.app/api?username=xSharkhy&theme=midnight-purple&show_icons=true&bg_color=0D1117&hide_border=true" />
-                </div>
+                <a href="">
+                    <div class="cardLink">
+                        <h4>Desarrollo de Interfaces Web</h4>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="cardLink">
+                        <h4>Desarrollo Web en Entorno Cliente</h4>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="cardLink">
+                        <h4>Desarrollo Web en Entorno Servidor</h4>
+                    </div>
+                </a>
+                <a href="">
+                    <div class="cardLink">
+                        <h4>Inglés Técnico II</h4>
+                    </div>
+                </a>
             </section>
         </article>
     </main>
