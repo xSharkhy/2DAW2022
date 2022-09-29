1edeffa5e51ee35427ad6525c05b94cc37b81fd1
xSharkhy <sharkhyacc@gmail.com>
2022-09-29 19:41:42 +0200

    arreglao

--- PHP/header.inc.php
+++ PHP/header.inc.php
@@ -1,10 +1,10 @@
 
 <header class="container header">
     <nav class="topnav navs">
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="../imatges/luffy.jpg" alt="Luffy" height="52px"></a>
-        <a href="/index.php" class="active">Inicio</a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="imatges/luffy.jpg" alt="Luffy" height="52px"></a>
+        <a href="/2DAW2022/index.php" class="active">Inicio</a>
         <a href="javascript:void(0)" class="dropdown">Asignaturas</a>
-        <a href="/recursos.php">Recursos</a>
-        <a href="">Comming next...</a>
+        <a href="/2DAW2022/recursos.php">Recursos</a>
+        <a href="">Comming soon...</a>
     </nav>
 </header>
