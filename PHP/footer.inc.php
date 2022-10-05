d2e181875104da8c939c96a1e63bf6e9e5b90c29
xSharkhy <sharkhyacc@gmail.com>
2022-10-05 20:08:24 +0200

    frontpage

--- PHP/footer.inc.php
+++ PHP/footer.inc.php
@@ -1,9 +1,9 @@
 
 <footer class="container footer">
     <nav class="botnav navs">
-        <a href="https://github.com/xSharkhy"><img src="/imatges/git.svg" alt="GIT Icon" height="36px"></a>
-        <a href="https://twitter.com/Sharkhy_"><img src="/imatges/twitter.svg" alt="Twitter Icon" height="36px"></a>
-        <a href="https://twitch.tv/Sharkho_"><img src="/imatges/twitch.svg" alt="Twitch Icon" height="36px"></a>
-        <a href="https://instagram.com/Sharkhy_"><img src="/imatges/instagram.svg" alt="Instagram Icon" height="36px"></a>
+        <a href="https://github.com/xSharkhy"><img src="imatges/git.svg" alt="GIT Icon" height="36px"></a>
+        <a href="https://twitter.com/Sharkhy_"><img src="imatges/twitter.svg" alt="Twitter Icon" height="36px"></a>
+        <a href="https://twitch.tv/Sharkho_"><img src="imatges/twitch.svg" alt="Twitch Icon" height="36px"></a>
+        <a href="https://instagram.com/Sharkhy_"><img src="imatges/instagram.svg" alt="Instagram Icon" height="36px"></a>
     </nav>
 </footer>
